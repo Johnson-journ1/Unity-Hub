5573fb4f0c77e2fea31e429d8f43fc750296df45
Johnson-journ1 <johnsonatitebi13@gmail.com>
2026-02-05 13:42:36 +0100

    bbbb

--- bells-university-app/frontend-vite/vite.config.js
+++ bells-university-app/frontend-vite/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_API_BASE_URL': process.env.VITE_API_BASE_URL
+  },
   plugins: [react()],
-  server: {
+  /*server: {
     proxy: {
       '/api': {
         target: API_BASE_URL,
         changeOrigin: true,
       },
     },
-  },
+  },*/
 })
