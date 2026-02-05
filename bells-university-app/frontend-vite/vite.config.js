934f04fa51cabfa3dedd7ff500355161f87087c9
Johnson-journ1 <johnsonatitebi13@gmail.com>
2026-02-05 12:58:38 +0100

    Blood of Jesus

--- bells-university-app/frontend-vite/vite.config.js
+++ bells-university-app/frontend-vite/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: API_BASE_URL,
         changeOrigin: true,
       },
     },
